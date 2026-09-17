ab0de29ce66637a70f18db9ad5e342342296fa38
j4z3-83 <jaldridge@byupathway.edu>
2026-09-17 10:04:43 -0600

    chamber

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -44,32 +44,35 @@ async function apiFetch() {
 
 function displayResults(data) {
     townName.innerHTML = `${data.city.name}`;
-    
+
     const dailyForecasts = data.list.filter(item => item.dt_txt.includes("21:00:00")).slice(0, 3);
-    
+
     const formatDate = (dateTxt) => {
         const dateObject = new Date(dateTxt);
         return dateObject;
     };
-    
+
     if (dailyForecasts.length >= 3) {
         day1.innerHTML = formatDate(dailyForecasts[0].dt_txt).toLocaleDateString('en-US', { weekday: 'long' });
         date1.innerHTML = formatDate(dailyForecasts[0].dt_txt).toLocaleDateString('en-US', { month: 'long', day: 'numeric' });
         temp1.innerHTML = `${dailyForecasts[0].main.temp}&deg;C`;
         icon1.setAttribute('src', `https://openweathermap.org/img/wn/${dailyForecasts[0].weather[0].icon}@2x.png`);
+        icon1.setAttribute('alt', dailyForecasts[0].weather[0].description);
         caption1.innerHTML = dailyForecasts[0].weather[0].description;
-        
+
         day2.innerHTML = formatDate(dailyForecasts[1].dt_txt).toLocaleDateString('en-US', { weekday: 'long' });
         date2.innerHTML = formatDate(dailyForecasts[1].dt_txt).toLocaleDateString('en-US', { month: 'long', day: 'numeric' });
         temp2.innerHTML = `${dailyForecasts[1].main.temp}&deg;C`;
         icon2.setAttribute('src', `https://openweathermap.org/img/wn/${dailyForecasts[1].weather[0].icon}@2x.png`);
+        icon2.setAttribute('alt', dailyForecasts[1].weather[0].description);
         caption2.innerHTML = dailyForecasts[1].weather[0].description;
-        
+
         day3.innerHTML = formatDate(dailyForecasts[2].dt_txt).toLocaleDateString('en-US', { weekday: 'long' });
         date3.innerHTML = formatDate(dailyForecasts[2].dt_txt).toLocaleDateString('en-US', { month: 'long', day: 'numeric' });
         temp3.innerHTML = `${dailyForecasts[2].main.temp}&deg;C`;
         icon3.setAttribute('src', `https://openweathermap.org/img/wn/${dailyForecasts[2].weather[0].icon}@2x.png`);
-        caption3.innerHTML = dailyForecasts[2].weather[0].description;    
+        icon3.setAttribute('alt', dailyForecasts[2].weather[0].description);
+        caption3.innerHTML = dailyForecasts[2].weather[0].description;
     }
 }
 
